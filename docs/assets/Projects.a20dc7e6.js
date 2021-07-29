36736dc80d82908b7fd47c3daea7bb54c870352d
naajunior <nelson.a.antunes@gmail.com>
2021-07-29 00:42:16 -0300

    minifying novamente

--- docs/assets/Projects.a20dc7e6.js
+++ docs/assets/Projects.a20dc7e6.js
@@ -1,11 +1,1 @@
-import { o as openBlock, c as createBlock, b as createVNode, t as toDisplayString } from "./vendor.89681dca.js";
-const _sfc_main = {};
-const _hoisted_1 = /* @__PURE__ */ createVNode("section", null, " \u{1F6A7} \u{1F3D7} \u{1F6A7} ", -1);
-function _sfc_render(_ctx, _cache) {
-  return openBlock(), createBlock("article", null, [
-    createVNode("h1", null, toDisplayString(_ctx.$t("projects")), 1),
-    _hoisted_1
-  ]);
-}
-_sfc_main.render = _sfc_render;
-export { _sfc_main as default };
+import{o as t,c as n,b as r,t as e}from"./vendor.89681dca.js";const o={},s=r("section",null," 🚧 🏗 🚧 ",-1);o.render=function(o,a){return t(),n("article",null,[r("h1",null,e(o.$t("projects")),1),s])};export{o as default};
